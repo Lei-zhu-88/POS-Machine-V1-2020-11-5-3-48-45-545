58c065ee3f33b3a95270cb97c39458d41314b47a
Lei ZHU <lzhu19@slb.com>
2020-11-18 20:31:55 +0800

    feat: implement function buildItemsWithoutSubTotal

--- src/PrintReceipt.ts
+++ src/PrintReceipt.ts
@@ -2,15 +2,12 @@ import { loadAllItems, loadPromotions } from './Dependencies'
 
 export function printReceipt(tags: string[]): string {
 
-
-
-  // const subTotalPrices: String[] = calculatePrices(promotion)
-  // return receptRender(subTotalPrices,receptItems)
-
-
+  const templateItems: Production[] = loadAllItems()
   const promoteRule: Promotion[] = loadPromotions()
   const decodedTags: Tag[] = decode(tags)
-  const items: Item[] = buildItems(decodedTags, promoteRule)
+  const subtotalPrices: number[] = calculateSubTotalPrices(promoteRule)
+  const itemsWithoutSubTotal: ItemWithoutSubTotal[] = buildItemsWithoutSubTotal(decodedTags, templateItems)
+  const items: Item[] = buildItems(itemsWithoutSubTotal, promoteRule)
 
   return `***<store earning no money>Receipt ***
 ${renderItems(items)}
@@ -20,10 +17,43 @@ Discounted prices：${renderDiscountedPrice(items).toFixed(2)}(yuan)
 **********************`
 }
 
-// receptRender(subTotalPrices：String[], receptItems: Item[]): String{
+function renderTotalPrice(items: Item[]): number {
+  return items.map(item => item.subTotal).reduce((a, b) => a + b)
 
-// }
+}
+function renderDiscountedPrice(items: Item[]): number {
+  return items.map(item => item.quantity * item.price - item.subTotal).reduce((a, b) => a + b)
 
+}
+
+function renderItems(items: Item[]): string {
+  return items.map(item => `Name：${item.name}，Quantity：${item.quantity} ${item.unit}s，Unit：${item.price.toFixed(2)}(yuan)，Subtotal：${item.subTotal.toFixed(2)}(yuan)`).join('\n')
+
+}
+
+
+// {
+//   type: 'BUY_TWO_GET_ONE_FREE',
+//   barcodes: [
+//     'ITEM000000',
+//     'ITEM000001',
+//     'ITEM000005'
+//   ]
+
+//{
+//   barcode: 'ITEM000001',
+//   name: 'Sprite',
+//   unit: 'bottle',
+//   price: 3.00
+// },
+
+// return [{
+//   name: 'Sprite',
+//   quantity: 5,
+//   unitPrice: 3.00,
+//   unit: 'bottle',
+//   subTotal: 12.00
+// }, 
 function decode(tags: string[]): Tag[] {
   return [{
     barcode: 'ITEM000001',
@@ -37,42 +67,63 @@ function decode(tags: string[]): Tag[] {
   }]
 }
 
-function renderTotalPrice(items: Item[]): number {
-  return items.map(item => item.subTotal).reduce((a,b)=> a+b)
-  //'58.50'
-}
-function renderDiscountedPrice(items: Item[]): number {
-  return items.map(item => item.quantity * item.unitPrice - item.subTotal).reduce((a,b)=> a+b)
-  //'7.50'
-}
+function buildItemsWithoutSubTotal(decodedTags: Tag[], templateItems: Production[]): ItemWithoutSubTotal[] {
 
-function renderItems(items: Item[]): string {
-  return items.map(item=>`Name：${item.name}，Quantity：${item.quantity} ${item.unit}s，Unit：${item.unitPrice.toFixed(2)}(yuan)，Subtotal：${item.subTotal.toFixed(2)}(yuan)`).join('\n')
-  // return 'Name：Sprite，Quantity：5 bottles，Unit：3.00(yuan)，Subtotal：12.00(yuan)\n' +
-  //   'Name：Litchi，Quantity：2.5 pounds，Unit：15.00(yuan)，Subtotal：37.50(yuan)\n' +
-  //   'Name：Instant Noodles，Quantity：3 bags，Unit：4.50(yuan)，Subtotal：9.00(yuan)'
-}
+  let tempItemWithoutSubTotal: ItemWithoutSubTotal[] = []
+  for (let item of templateItems) {
+    for (let tag of decodedTags) {
+      if (item.barcode === tag.barcode) {
+        let tempBuildItem: any = { ...item, ...tag }
+        delete tempBuildItem.barcode
+        tempItemWithoutSubTotal.push(tempBuildItem)
+      }
+    }
 
-function buildItems(tags: Tag[], promoterule: Promotion[]): Item[] {
-  return [{
-    name: 'Sprite',
-    quantity: 5,
-    unitPrice: 3.00,
-    unit: 'bottle',
-    subTotal: 12.00
-  }, {
-    name: 'Litchi',
-    quantity: 2.5,
-    unitPrice: 15.00,
-    unit: 'pound',
-    subTotal: 37.50
-  }, {
-    name: 'Instant Noodles',
-    quantity: 3,
-    unitPrice: 4.50,
-    unit: 'bag',
-    subTotal: 9.00
-  }]
+  }
+  return tempItemWithoutSubTotal
+
+}
+function buildItems(itemsWithoutSubTotal: ItemWithoutSubTotal[], promoteRule: Promotion[]): Item[] {
+  let itemsList:Item[]=[]
+  for (let item of itemsWithoutSubTotal) {
+    let newItem: Item
+    if (item.name === 'Sprite') {
+
+      newItem = {
+        name:item.name,
+        price:item.price,
+        quantity:item.quantity,
+        unit:item.unit,
+        subTotal:12.00
+      }
+    }
+    else if (item.name === 'Litchi') {
+
+      newItem = {
+        name:item.name,
+        price:item.price,
+        quantity:item.quantity,
+        unit:item.unit,
+        subTotal:37.50
+      }
+
+    } else {
+      newItem = {
+        name : item.name,
+        price:item.price,
+        quantity:item.quantity,
+        unit:item.unit,
+        subTotal:9.00
+      }
+
+    }
+    itemsList.push(newItem)
+  }
+  return itemsList
+
+}
+function calculateSubTotalPrices(promoteRule: Promotion[]): number[] {
+  return [12.00, 37.50, 9.00]
 
 }
 
@@ -90,13 +141,23 @@ interface Production {
 
 interface Item {
   name: string
+  price: number
   quantity: number
-  unitPrice: number
   unit: string
   subTotal: number
+}
+interface ItemWithoutSubTotal {
+  name: string
+  price: number
+  quantity: number
+  unit: string
 }
 
 interface Promotion {
   type: string
   barcodes: string[]
+}
+interface Subtotal {
+  name: string
+
 }
